61c0e137f4af31f3cfaec94da65115d6ca799aff
GGo-ri <speedmult@naver.com>
2026-07-30 14:40:45 +0900

    테이블 수정

--- makesql.sql
+++ makesql.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS jerseydb DEFAULT CHARACTER SET utf8mb4;
 USE jerseydb;
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     role VARCHAR(20) NOT NULL DEFAULT 'GUEST'
@@ -14,7 +14,7 @@ VALUES ('GGori', 'GGori0516', 'ADMIN')
 ON DUPLICATE KEY UPDATE password = VALUES(password), role = VALUES(role);
 
 CREATE TABLE IF NOT EXISTS jerseys (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     serial_number VARCHAR(10) UNIQUE NOT NULL,
     back_number INT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS jerseys (
 
 INSERT INTO jerseys (serial_number, back_number, product_name, jersey_type, stock, price)
 VALUES
-	('10090', 9, '엘링 홀란(MCI)', 'HOME', 20, 195000)
+    ('10090', 9, '맨체스터 시티 엘링 홀란', 'HOME', 20, 195000)
 ON DUPLICATE KEY UPDATE
-	stock = VALUES(stock),
+    stock = VALUES(stock),
     price = VALUES(price);
