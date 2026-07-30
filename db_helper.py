7171e33d927a4fbf18784ac5370fdd5170ac4963
GGo-ri <speedmult@naver.com>
2026-07-30 15:05:08 +0900

    get jersey의 매개변수 수정

--- db_helper.py
+++ db_helper.py
@@ -31,7 +31,7 @@ class DB :
         return res["cnt"] == 1
 
   # 조회
-  def get_jerseys(self, team_keyword) :
+  def get_jerseys(self, team = "") :
     sql = """
         SELECT id, serial_number, back_number, product_name, jersey_type, stock, price
         FROM jerseys
